2bbda9d00bc02b49a83a34a0f92f3694148105d2
yesrin <asjs11@naver.com>
2023-07-14 23:14:45 +0900

    JwtAuthorizationFilter 구현

--- src/main/java/com/example/memo/configuration/security/JwtAuthorizationFilter.java
+++ src/main/java/com/example/memo/configuration/security/JwtAuthorizationFilter.java
@@ -1,18 +1,73 @@
 package com.example.memo.configuration.security;
 
+import io.jsonwebtoken.Claims;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+@Slf4j
 class JwtAuthorizationFilter extends OncePerRequestFilter {
+	private final JwtUtil jwtUtil;
+	private final UserDetailsService userDetailsService;
+
+	JwtAuthorizationFilter(JwtUtil jwtUtil, UserDetailsService userDetailsService) {
+		this.jwtUtil = jwtUtil;
+		this.userDetailsService = userDetailsService;
+	}
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
 		FilterChain filterChain) throws ServletException, IOException {
+
 		// TODO : 요청에 들어온 JWT를 parsing해서 "ROLE_MEMBER" 권한이 있는지 확인하고, SecurityContextHolder에 context 설정하기
-		filterChain.doFilter(request, response);
+
+		//권한있는지 확인
+		String tokenValue = jwtUtil.getTokenFromHeader(request);
+
+		if (StringUtils.hasText(tokenValue)) {
+
+			if (!jwtUtil.validateToken(tokenValue)) {
+				log.error("Token Error");
+				return;
+			}
+
+			Claims info = jwtUtil.getUserInfoFromToken(tokenValue);
+
+			try {
+				setAuthentication(info.getSubject());
+			} catch (Exception e) {
+				log.error(e.getMessage());
+				return;
+			}
+		}
+
+	}
+
+	//context 설정
+	public void setAuthentication(String username) {
+		SecurityContext context = SecurityContextHolder.createEmptyContext();
+		Authentication authentication = createAuthentication(username);
+		context.setAuthentication(authentication);
+
+		SecurityContextHolder.setContext(context);
+	}
+
+
+	// 인증 객체 생성
+	private Authentication createAuthentication(String username) {
+		UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+		return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
 	}
 }
